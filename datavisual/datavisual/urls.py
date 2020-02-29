33474d42b6e05c569fd20fc26a26bb6f12720f32
jjack27 <yizhouzhao27@gmail.com>
2020-02-28 18:36:57 -0700

    connect establised app

--- datavisual/datavisual/urls.py
+++ datavisual/datavisual/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from visual import views as visual_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', visual_view.index),
 ]
