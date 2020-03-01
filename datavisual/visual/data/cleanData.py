d0f194491b3d6148c5a5662f847101c9685d1b01
jjack27 <yizhouzhao27@gmail.com>
2020-02-29 19:38:57 -0700

    update read me

--- datavisual/visual/data/cleanData.py
+++ datavisual/visual/data/cleanData.py
@@ -5,6 +5,8 @@ def main():
     path = './investments.csv'
     unclean_data = pd.read_csv(path, encoding='unicode-escape')
     
+
+
     # drop na rows if name is empty
     unclean_data = unclean_data[unclean_data['name'].notna()]
     print(unclean_data.isna().sum())
