012c5b02b59b82999d2d2bbc75d9cc6f23d314c0
jjack27 <yizhouzhao27@gmail.com>
2020-03-03 22:08:24 -0700

    add clean data

--- datavisual/data/cleanData.py
+++ datavisual/data/cleanData.py
@@ -3,19 +3,29 @@ import pandas as pd
 
 def main():
     path = './investments.csv'
+
+
     unclean_data = pd.read_csv(path, encoding='unicode-escape')
     
 
-
     # drop na rows if name is empty
     unclean_data = unclean_data[unclean_data['name'].notna()]
-    #print(unclean_data.isna().sum())
     
-    unique = unclean_data["market"].unique()
-    for value in unique:
-        if value == nan:
-            print(value)
+    
+    # set nan in market to unknown
+    columns_to_fill_na = ['market', 'category_list', 'homepage_url', 'status', \
+        'country_code', 'region', 'city', 'founded_at', 'founded_year', 'state_code']
+    columns_to_drop = ['founded_month', 'founded_quarter']
 
+    # drop columns
+    for col in columns_to_drop:
+        unclean_data = unclean_data.drop(columns = col)
+    
+    # fill na to "Unknown"
+    for col in columns_to_fill_na:
+        unclean_data[col] = unclean_data[col].fillna("Unknown")
+
+    unclean_data.to_csv(r'./cleaned_data.csv', index = False)
 
 if __name__ == "__main__":
     main()
