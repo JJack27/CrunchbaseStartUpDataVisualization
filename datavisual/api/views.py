94028f2ec7117e46666129920096e65f8233efec
JJack27 <yizhouzhao27@gmail.com>
2020-03-05 21:30:58 -0700

    fixed api to manage unknown

--- datavisual/api/views.py
+++ datavisual/api/views.py
@@ -24,12 +24,14 @@ import requests
 from django.conf import settings
 
 # return a data frame of selected features and counts
-def get_counts(df, filters, unknown):
+def get_counts(df, filters, unknown, metric):
 
     filtered_data = df.copy()
     
     equal_filter_cols = ['category_list', 'homepage_url', 'status', \
         'country_code', 'region', 'city', 'founded_at',  'state_code']
+
+    # remove unknown from the dataset if applicable
     if not unknown:
         for key in filters.keys():
             if key in equal_filter_cols or key == 'market':
@@ -37,6 +39,7 @@ def get_counts(df, filters, unknown):
             else:
                 filtered_data = filtered_data[filtered_data[key] != 0]
     
+    # retrieve data from the dataset based on given filter
     for fil, threshold in filters.items():
         if fil == "market":
             filtered_data = filtered_data[filtered_data['market_count'] > threshold]
@@ -46,47 +49,86 @@ def get_counts(df, filters, unknown):
         else:
             filtered_data = filtered_data[filtered_data[fil] > threshold]
 
-    groups = filtered_data.groupby(list(filters.keys()))['market']
-    counts = groups.count()
+    # return based on metric
+    if (metric == 'count'):
+        groups = filtered_data.groupby(list(filters.keys()))['market']
+        result = groups.count()
+    else:
+        groups = filtered_data.groupby(list(filters.keys()))['funding_total_usd']
+        result = groups.sum()
+    return result
 
-    
-    return counts
+# Given node of a tree, return the sum of values of the sub-tree
+def sum_of_subtree(root):
+    if 'value' in root.keys():
+        return root['value']
+    summation = 0
+    for key in root.keys():
+        if key == 'name':
+            continue
+        summation += sum_of_subtree(root[key])
+    return summation
 
 def generate_tree(data):
     # first generate index
     label = data.index.labels
     levels = data.index.levels
+    index_names = data.index.names
+    
+    # declare helper variables
+    columns_to_fill_na_unknown = ['market', 'category_list', 'homepage_url', 'status', \
+        'country_code', 'region', 'city', 'founded_at',  'state_code']
+    columns_to_fill_na_zero = ['founded_year']
+
+    # Merge index to keys
     keys = [] 
     for i in range(len(label[0])):
         keys.append([levels[0][label[0][i]],levels[1][label[1][i]],levels[2][label[2][i]]])
     
+    # Constructing intermediate tree for easily formatting
     tree = {}
     for key in keys:
+        # first layer
         if key[0] not in tree.keys():
             tree[key[0]] = {'name':key[0]}
-            
+           
+        # second layer
         if key[1] not in tree[key[0]].keys():
             tree[key[0]][key[1]] = {'name':key[1]}
         
+        # third layer
         if key[2] not in tree[key[0]][key[1]].keys():
             tree[key[0]][key[1]][key[2]] = {'name': key[2], 'value':data[key[0]][key[1]][key[2]]}#, 'value':data[key[0]][key[1]][key[2]]
     
+    # Constructing tree to return
     re_tree = {'name': 'Root', 'children': [], 'children_names':[]}
-    
     for key in tree.keys():
+        # first layer
         child_dic1 = {'name':key, 'children':[], 'children_names':[]}
         if tree[key]['name'] not in re_tree['children_names']:
-            re_tree['children_names'].append(key)
-            re_tree['children'].append(child_dic1)
+            # directly set value of unknown
+            if (key == 'Unknown' and index_names[0] in columns_to_fill_na_unknown) or (key == 0 and index_names[0] in columns_to_fill_na_zero):
+                value_of_unknown = sum_of_subtree(tree[key])
+                re_tree['children'].append({'name':'Unknown', 'value':value_of_unknown})
+            else:
+                re_tree['children_names'].append(key)
+                re_tree['children'].append(child_dic1)
         
+        # Second layer
         for key2 in tree[key].keys():
             if key2 == 'name':
                 continue
             child_dic2 = {'name':key2, 'children':[], 'children_names':[]}
             if key2 not in child_dic1['children_names']:
-                child_dic1['children_names'].append(key2)
-                child_dic1['children'].append(child_dic2)
+                # Directly set value of unknown
+                if (key2 == 'Unknown' and index_names[1] in columns_to_fill_na_unknown) or (key2 == 0 and index_names[1] in columns_to_fill_na_zero):
+                    value_of_unknown = sum_of_subtree(tree[key][key2])
+                    child_dic1['children'].append({'name':'Unknown', 'value':value_of_unknown})    
+                else:
+                    child_dic1['children_names'].append(key2)
+                    child_dic1['children'].append(child_dic2)
 
+            # Third layer
             for key3 in tree[key][key2].keys():
                 if key3 == 'name':
                     continue
@@ -94,8 +136,6 @@ def generate_tree(data):
                 if key3 not in child_dic2['children_names']:
                     child_dic2['children_names'].append(key3)
                     child_dic2['children'].append(child_dic3)
-                    
-
     return re_tree
 
 
@@ -130,10 +170,9 @@ class FilterAPI(APIView):
         
         # generate tree structure of for data visualizing
         data = {'name':'root'}
-        if label_name == 'count':
-            children = get_counts(df, filters, unknown)
+        children = get_counts(df, filters, unknown, label)
+        print(children)
 
-        #print(children.index)
         tree = generate_tree(children)
         response['tree'] = tree
         return Response(response, status = 200)
