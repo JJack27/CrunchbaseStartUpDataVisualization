928ba921cad67f53de6778c65704696b0954b5f8
jjack27 <yizhouzhao27@gmail.com>
2020-03-16 22:19:38 -0600

    deploy

--- datavisual/datavisual/settings.py
+++ datavisual/datavisual/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)	
 # https://docs.djangoproject.com/en/2.1/howto/static-files/	
 
-STATIC_ROOT = "/visual/static/"
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
